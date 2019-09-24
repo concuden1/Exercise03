66f4be1a8ab3a37c7408f7bca7e2f094d7b09ae0
nhathao <nhathaotrann21@gmail.com>
2019-09-24 15:32:17 +0700

    DrawTriangle

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,8 +10,49 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello new world");
+           DrawTriangle(4,'*');
+            Console.ReadLine();
             
+        }
+        static void HLine(int n, char c )
+        {
+            for (int i = 0; i < n; i++)          
+                Console.Write(c);          
+        }
+        static void VLine(int n, Char c)
+        {
+            for (int i = 0; i < n; i++)         
+                Console.WriteLine(c);           
+        }
+        static void DLine(int n, char c)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                HLine(i - 1, ' ');
+                Console.WriteLine(" " + n);
+            }
+        }
+        //first line: n-1 spaces, 1 star
+        //second line: n-2 spaces, 1 star, 1 space, 1 star
+        //third line: n-3 spaces, 1 star, 3 spaces, 1 star
+        //fourth line: n-4 spaces, 1 star, 5 spaces, 1 star
+        // last line: 2n-1 stars
+        static void DrawTriangle(int n, char c)
+        {
+           
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = i; j < n; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= (2 * i - 1); j++)
+                {
+                    Console.Write(c);
+                }
+                Console.WriteLine("");
+
+            }
         }
     }
 }
